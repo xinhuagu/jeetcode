dcfde42bbd3da4cdcee542a4dae9cd722ccd8ed7
Xinhua Gu <xinhua.gu@gmail.com>
2022-05-23 22:21:36 +0200

    2. solution

--- src/middle-of-the-linked-list.java
+++ src/middle-of-the-linked-list.java
@@ -25,3 +25,20 @@ class Solution {
     return head;
   }
 }
+
+/**
+ * solution 2.
+ */
+
+class Solution {
+  public ListNode middleNode(ListNode head) {
+    var slow = head;
+    var fast = head;
+
+    while(fast!=null && fast.next!=null){
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+}
