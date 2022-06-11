d1d3838e940e787149fa24d0f3a84b5e711238af
Xinhua Gu <xinhua.gu@gmail.com>
2022-06-11 11:34:32 +0200

    linked -list cycle

---
+++ src/linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    Set set = new HashSet<ListNode>();
+    while(head != null){
+      if( set.contains(head)){
+        return true;
+      }
+      set.add(head);
+      head = head.next;
+    }
+    return false;
+
+  }
+}
